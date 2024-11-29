351b2ecd1357053941c465f4e275f55a81026b56
MD Yusuf <yk7963838@gmail.com>
2024-11-29 19:17:16 +0600

    Your commit message

--- app/admin/Layout.jsx
+++ app/admin/Layout.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import SideBar from '../componenets/Admin components/SideBar'
+import Image from "next/image";
+import SideBar from "../componenets/Admin components/SideBar";
+import { assets } from "@/Assets/assets";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <>
-    <div className='flex'>
-      <SideBar />
-    </div>
-    {children}
+      <div className="flex">
+        <SideBar />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+            <h3 className="font-medium">Admin Panel</h3>
+            <Image src={assets.profile_icon} width={40} alt="profile_icon" />
+          </div>
+          {children}
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+"use client";
+import { assets } from "@/Assets/assets";
+import Image from "next/image";
+import React, { useState } from "react";
 
-export default function page() {
+export default function Page() {
+  const [image, setImage] = useState(null);
+
   return (
-    <div>page</div>
-  )
+    <>
+      <form className="pt-5 px-5 sm:pt-12 sm:pl-16">
+        <p className="text-xl">Upload thumbnail</p>
+        <label htmlFor="image">
+          <Image
+            src={image ? URL.createObjectURL(image) : assets?.upload_area}
+            width={140}
+            height={140}
+            alt="uploadAreaImage"
+          />
+        </label>
+        <input
+          onChange={(e) => setImage(e.target.files[0])}
+          type="file"
+          id="image"
+          hidden
+          required
+        />
+        <p className="text-xl mt-4">Blog Title</p>
+        <input
+          type="text"
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          placeholder="Tyep Here"
+          id=""
+        />
+        <p className="text-xl mt-4">Blog Description</p>
+        <textarea
+          type="text"
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+          placeholder="Text Here"
+          rows={6}
+          id=""
+        />
+        <p className="text-xl mt-4">Blog Category</p>
+        <select name="category" className="w-40 mt-4 px-4 py-3 border text-gray-500">
+          <option value='Startup'>Startup</option>
+          <option value='Technology'>Technology</option>
+          <option value='Lifestyle'>Lifestyle</option>
+        </select>
+        <br />
+        <button type="submit" className="mt-8 w-40 h-12 bg-black text-white">ADD BLOG</button>
+      </form>
+    </>
+  );
 }
